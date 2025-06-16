171ef120cf95254d759c07a09e6dbcf70ee9df6d
Steven Masley <Emyrk@users.noreply.github.com>
2025-06-16 08:31:14 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,5 @@
 terraform {
   required_version = ">= 1.0"
-
-  required_providers {
-    coder = {
-      source  = "coder/coder"
-      version = ">= 2.5"
-    }
-  }
 }
 
 # This module comes with 5mb of data
